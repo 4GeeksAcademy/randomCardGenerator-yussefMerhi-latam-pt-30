f645ad2be70468018fe8b515d6689e2298cadf4c
Yussef Merhi <101605162+Annorakx@users.noreply.github.com>
2024-04-22 14:37:41 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,62 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const generateRandomCard = () => {
+    let suit = ["♠", "♥", "♣", "♦"];
+    let numberCard = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    const generateRandomArrayValue = array => {
+      return array[Math.floor(Math.random() * array.length)];
+    };
+
+    const cardNumberElement = document.getElementById("numberInCard");
+    cardNumberElement.innerHTML = generateRandomArrayValue(numberCard);
+
+    let randomSuit = generateRandomArrayValue(suit);
+    const color = randomSuit === "♥" || randomSuit === "♦" ? "red" : "black";
+    const cardSuitElement = document.querySelectorAll(".suitInCard");
+    cardSuitElement.forEach(suit => {
+      suit.style.color = color;
+      suit.innerHTML = randomSuit;
+    });
+  };
+  generateRandomCard();
+
+  const randomCardButton = document.getElementById("randomCardButton");
+  randomCardButton.addEventListener("click", generateRandomCard);
+
+  setInterval(checkFunction, 10000);
+  function checkFunction() {
+    let check = document.getElementById("flexCheck");
+    if (check.checked === true) generateRandomCard();
+  }
+
+  const changeCardSize = () => {
+    const card = document.querySelector(".card");
+    const rangeValue = document.getElementById("cardSizeRange").value;
+
+    card.style.width = rangeValue * 10 + "px";
+    if (parseInt(rangeValue * 10) < 100) {
+      card.style.width = "100px";
+    } else if (parseInt(rangeValue * 10) > 650) {
+      card.style.width = "650px";
+    }
+  };
+
+  const cardSizeRange = document.getElementById("cardSizeRange");
+  cardSizeRange.addEventListener("input", changeCardSize);
 };
